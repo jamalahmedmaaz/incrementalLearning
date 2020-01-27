26f3fbccc5cb01fccc64043ab033994ce81c6abb
Jamal <jamalahmedmaaz@gmail.com>
2020-01-27 06:56:26 +0530

    Jan 26 and 27

--- daily/src/main/java/January/jan27/Behaviour.java
+++ daily/src/main/java/January/jan27/Behaviour.java
@@ -1,4 +1,10 @@
 package January.jan27;
 
 public class Behaviour {
+
+    /**
+     * 1. There is no FRUIT without risk.
+     * 2. Look at all people who are successful in there ART form - they all do take risk.
+     * 3. Treat your work as an ART form.
+     */
 }
