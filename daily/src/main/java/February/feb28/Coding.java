a3aa4fc1c529b8e4ae5b9a4a67a4f62fcd32b02d
Jamal <jamalahmedmaaz@gmail.com>
2020-02-28 17:34:12 +0530

    Feb 28:
    Facebook

--- daily/src/main/java/February/feb28/Coding.java
+++ daily/src/main/java/February/feb28/Coding.java
@@ -1,5 +1,9 @@
 package February.feb28;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 public class Coding {
     class LowestCommonAccesstorOfDeepestLeaves {
         TreeNode result;
@@ -30,5 +34,66 @@ public class Coding {
 
         public TreeNode left;
         public TreeNode right;
+    }
+
+    class StringTransformsIntoAnotherString {
+        public boolean canConvert(String str1, String str2) {
+            if (str1.equals(str2)) {
+                return true;
+            }
+            if (str1.length() != str2.length()) {
+                return false;
+            }
+            Map<Character, Character> map = new HashMap();
+
+            for (int i = 0; i < str1.length(); i++) {
+                char c1 = str1.charAt(i);
+                char c2 = str2.charAt(i);
+                if (map.getOrDefault(c1, c2) != c2) {
+                    return false;
+                }
+                map.put(c1, c2);
+            }
+            Set<Character> set = new HashSet(map.values());
+            return set.size() < 26;
+        }
+    }
+
+    class MaximalSquare {
+        public int maximalSquare(char[][] matrix) {
+            if (matrix == null || matrix.length == 0) {
+                return 0;
+            }
+            int r = matrix.length;
+            int c = matrix[0].length;
+            int[][][] dp = new int[r + 1][c + 1][3]; //TOP LEFT Hight/Width
+            int size = 0;
+            // Top = 0 , left = 1, dia = 2
+            for (int i = 1; i <= r; i++) {
+                for (int j = 1; j <= c; j++) {
+                    if (matrix[i - 1][j - 1] == '1') {
+                        dp[i][j][0] = dp[i - 1][j][0] + 1;
+                        dp[i][j][1] = dp[i][j - 1][1] + 1;
+
+                        int top = dp[i][j][0];
+                        int left = dp[i][j][1];
+
+                        /**
+                         * Diagnol should be set by top and left.
+                         */
+                        int diagMin = Math.min(left, top);
+                        dp[i][j][2] = Math.min(diagMin, dp[i - 1][j - 1][2] + 1);
+                        int diag = dp[i][j][2];
+
+                        int min = Math.min(left, Math.min(top, diag));
+
+                        //Min between top, left and diag is going to be the area parameter.
+                        // min*min = will be the answer.
+                        size = Math.max(size, min * min);
+                    }
+                }
+            }
+            return size;
+        }
     }
 }
