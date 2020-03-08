fdc4a9c189ffde784b73cff85fcfe5408ad476c1
Jamal <jamalahmedmaaz@gmail.com>
2020-03-08 21:34:02 +0530

    March 8
    Facebook

--- daily/src/main/java/March/march08/Coding.java
+++ daily/src/main/java/March/march08/Coding.java
@@ -1,5 +1,8 @@
 package March.march08;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 public class Coding {
     class ConstructMaximumBinaryTree_654 {
         int[] A;
@@ -60,6 +63,65 @@ public class Coding {
 
         public TreeNode(int val) {
             this.val = val;
+        }
+    }
+
+    class GetAllElements {
+        List<Integer> result = new ArrayList();
+        Stack<TreeNode> s1 = new Stack();
+        Stack<TreeNode> s2 = new Stack();
+
+        public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+            populate(root1, s1);
+            populate(root2, s2);
+            while (!s1.isEmpty() && !s2.isEmpty()) {
+                TreeNode r1 = s1.peek();
+                TreeNode r2 = s2.peek();
+                if (r1.val > r2.val) {
+                    result.add(r2.val);
+                    s2.pop();
+                    populate(r2.right, s2);
+                } else {
+                    result.add(r1.val);
+                    s1.pop();
+                    populate(r1.right, s1);
+                }
+
+            }
+            Stack<TreeNode> s = s1.isEmpty() ? s2 : s1;
+            while (!s.isEmpty()) {
+                TreeNode r = s.pop();
+                result.add(r.val);
+                populate(r.right, s2);
+            }
+            return result;
+        }
+
+        public void populate(TreeNode root, Stack<TreeNode> s) {
+            if (root == null) {
+                return;
+            }
+            s.add(root);
+            populate(root.left, s);
+        }
+    }
+
+    class maxProduct {
+        long result = 0, totalSum = 0, subTotal;
+
+        public int maxProduct(TreeNode root) {
+            totalSum = rec(root);
+            rec(root);
+            return (int) (result % (int) (1e9 + 7));
+        }
+
+        private long rec(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            subTotal = root.val + rec(root.left) + rec(root.right);
+            result = Math.max(result, subTotal * (totalSum - subTotal));
+            return subTotal;
         }
     }
 }
